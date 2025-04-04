647c31ffcaab622ba82a49525d75f29bf5a2d7be
enzo <lsalamante@yondu.com>
2025-04-04 16:49:37 +0800

    add ContactCountStartingNode component with styling and functionality

---
+++ src/components/ContactCountStartingNode.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { Handle, Position } from "@vue-flow/core";
+import { CirclePlay } from "lucide-vue-next";
+</script>
+
+<template>
+  <div
+    class="border-[#18E2CE] border-2 bg-[#E8FCFA] rounded-lg p-3 text-xs flex flex-row gap-2 items-center justify-center"
+  >
+    <CirclePlay color="#119e90" />
+    <p class="font-satoshi font-medium leading-[18px]">
+      2.5 billion contacts<br />will enter this journey
+    </p>
+
+    <Handle type="source" :position="Position.Bottom" :id="'source-bottom'" />
+  </div>
+</template>
